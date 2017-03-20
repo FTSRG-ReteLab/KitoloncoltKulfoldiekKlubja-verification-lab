a4627117fc24b266bdd85730459c4b1ea9017f6d
meres <rolaca12@gmail.com>
2017-03-20 07:17:35 -0700

    Implemented timer to automatically call followSpeed() every mseconds

--- train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainController.java
+++ train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainController.java
@@ -1,6 +1,10 @@
 package hu.bme.mit.train.interfaces;
 
+import java.util.List;
+
 public interface TrainController {
+
+	public void addToMonitor(Object me);
 
 	void followSpeed();
 
@@ -9,5 +13,9 @@ public interface TrainController {
 	void setSpeedLimit(int speedLimit);
 
 	void setJoystickPosition(int joystickPosition);
+
+	void startTimer();
+
+	void stopTimer();
 
 }
